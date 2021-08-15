f3a4e56e9aee2ba963a3c9a3d876d9546b67eaff
Luiz Libardi <luizlibardi@hotmail.com>
2021-08-15 15:30:27 -0300

    Finalizado comentado.

--- diagnostico.py
+++ diagnostico.py
@@ -1,7 +1,13 @@
+"""
+Diagnóstico proposto na aula de Estrutura de Dados 1 - UCL, pelo professor André Ribeiro no dia 05/08 para saber o nível da turma.
+"""
+
+# Entrada de dados pelo Usuário
 eleitores_consultados = int(input('Quantos eleitores foram consultados? '))
 votos_branco = int(input('Quantos votos foram em branco? '))
 indecisos = int(input('Quantos votos foram indecisos? '))
 
+# Classe para a criação dos candidatos
 class Candidatos:
     codigo = 1
 
@@ -10,17 +16,22 @@ class Candidatos:
         self._nome = nome
         self._intencoes = intencoes
 
+    # Função que retorna o código do candidato
     def get_codigo(self):
         return self._codigo
     
+    # Função que retorna o nome do candidato
     def get_nome(self):
         return self._nome
 
+    # Função que retorna a quantidade de intenções de votos do candidato
     def get_intencoes(self):
         return self._intencoes
 
 candidatos = []
 codigo = 1
+
+# Loop para o usuário fazer a criação dos candidatos
 while codigo != 0:
     codigo = int(input('\nCódigo do candidato: '))
     if codigo == 0:
@@ -31,18 +42,21 @@ while codigo != 0:
         candidato = Candidatos(codigo, nome, intencoes)
         candidatos.append(candidato)
 
+# Função que retorna o total de votos
 def total_votos():
     return eleitores_consultados + votos_branco + indecisos
 
+# Função que retorna o a porcentagem de votos em branco
 def percentual_branco():
     return f'{((votos_branco * 100) / total_votos())}% - Votos em Branco'
 
+# Função que retorna o a porcentagem de votos indecisos
 def percentual_indecisos():
     return f'{((indecisos * 100) / total_votos())}% - Indecisos\n'
 
 lista_votos = []
 
-# Letra A)
+# Letra A) Retorno '%Votos - Nome'
 for candidato in candidatos:
     percentual_voto = ((candidato.get_intencoes() * 100) / total_votos()) 
     print(f'{percentual_voto}% - {candidato.get_nome()}')
