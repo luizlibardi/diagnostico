d22f146d3297127bf49f0a649f91a8dfb2118bc1
Luiz Libardi <luizlibardi@hotmail.com>
2021-08-15 02:34:15 -0300

    agora com funções

--- diagnostico.py
+++ diagnostico.py
@@ -3,15 +3,19 @@ votos_branco = int(input('Quantos votos foram em branco? '))
 indecisos = int(input('Quantos votos foram indecisos? '))
 
 class Candidatos:
-
+    
     def __init__(self, codigo, nome, intencoes):
         self.codigo = codigo
         self.nome = nome
         self.intencoes = intencoes
 
+    def dict_intencoes(self):
+        dict = {'nome': self.nome, 'intencoes': self.intencoes}
+        print(dict)
+        
+
 candidatos = []
 codigo = 1
-
 while codigo != 0:
     codigo = int(input('\nCódigo do candidato: '))
     if codigo == 0:
@@ -23,14 +27,26 @@ while codigo != 0:
         candidatos.append(candidato)
 
 total_votos = eleitores_consultados + votos_branco + indecisos
-percentual_branco = ((votos_branco * 100) / total_votos)
-percentual_indecisos = ((indecisos * 100) / total_votos)
+
+def percentual_branco(votos_branco, total_votos):
+    percentual_branco = ((votos_branco * 100) / total_votos)
+    print(f'{percentual_branco}% - Votos em Branco')
+
+def percentual_indecisos(indecisos, total_votos):
+    percentual_indecisos = ((indecisos * 100) / total_votos)
+    print(f'{percentual_indecisos}% - Indecisos\n')
 
 for candidato in candidatos:
     percentual_voto = ((candidato.intencoes * 100) / total_votos)
     print(f'{percentual_voto}% - {candidato.nome}')
-print(f'{percentual_branco}% - Votos em Branco')
-print(f'{percentual_indecisos}% - Indecisos\n')
+    
+    if percentual_voto > 50:
+        print(f'O candidato {candidato.nome} possui alta probabilidade de eleição no primeiro turno')
+    #elif max(percentual_voto) : 
 
-if percentual_voto > 50:
-    print(f'O candidato {candidato.nome} possui alta probabilidade de eleição no primeiro turno')
+# candidato.dict_intencoes()
+percentual_branco(votos_branco, total_votos)
+percentual_indecisos(indecisos, total_votos)
+
+
+
